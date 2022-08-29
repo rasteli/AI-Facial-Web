ed3226b37f19322e237139c9656a0860f7376249
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2022-08-29 17:29:44 -0300

    Removed irrelevant scripts

--- src/components/UserDetail.tsx
+++
@@ -1,7 +0,0 @@
-import { useParams } from "react-router-dom"
-
-export function UserDetail() {
-  const { id } = useParams()
-
-  return <div>User ID: {id}</div>
-}
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import { CreateUserForm } from "./components/CreateUserForm"
-import { UserDetail } from "./components/UserDetail"
 
 export function AppRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<CreateUserForm />} />
-        <Route path="/users/:id" element={<UserDetail />} />
       </Routes>
     </BrowserRouter>
   )
