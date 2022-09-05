ae3cac0fe099bf01d08d6cc33970fd59778f43c1
Gabriel Rasteli <gabrielrasteli3@gmail.com>
2022-09-05 04:12:39 -0300

    Temporary webcam route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import { AppRoutes } from "./routes"
 import { useAuth } from "./contexts/AuthContext"
 
-import { Webcam } from "./components/Webcam"
-
 function App() {
   const { isLoading } = useAuth()
-
-  return <Webcam />
 
   return isLoading ? <div>Loading...</div> : <AppRoutes />
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import { Home } from "./components/Home"
 import { Login } from "./components/Login"
+import { Webcam } from "./components/Webcam"
 import { PrivateRoute } from "./components/PrivateRoute"
 import { CreateUserForm } from "./components/CreateUserForm"
 
@@ -18,6 +19,7 @@ export function AppRoutes() {
           }
         />
         <Route path="/login" element={<Login />} />
+        <Route path="/webcam" element={<Webcam />} />
         <Route path="/signup" element={<CreateUserForm />} />
       </Routes>
     </BrowserRouter>
